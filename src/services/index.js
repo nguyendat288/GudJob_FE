3f71e73f3b5eafe651ffbea1f9603392f358af65
khanhndhe153638 <khanhndhe153638@fpt.edu.vn>
2024-06-22 00:24:20 +0700

    remove secret code

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,2 @@
 export const BASE_URL = 'https://app-doan.azurewebsites.net'
 export const CLIENT_ID = "381550384992-htco6lopr3hah1jpn9u7qh3rcklqe6st.apps.googleusercontent.com"
-export const CLIENT_SECRET = "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"
-export const REDIRECT_URI = ['http://localhost:3000', 'http://localhost']
