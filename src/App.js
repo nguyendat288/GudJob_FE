e2114f9ce465f5a26804133e917fe020b7d844dd
khanhndhe153638 <khanhndhe153638@fpt.edu.vn>
2024-06-22 00:30:27 +0700

    create env file

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from '@mui/material'
 import { theme } from './themes/theme'
 import { Helmet } from "react-helmet";
-import { CLIENT_ID } from "./services";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 function App() {
 
