ea6f4515b39d5495abe6782a94ddb5f8a33102aa
khanhndhe153638 <khanhndhe153638@fpt.edu.vn>
2024-07-14 17:14:37 +0700

    update layout, update custom overlay

--- src/pages/Admin/component/userList.jsx
+++ src/pages/Admin/component/userList.jsx
@@ -8,6 +8,101 @@ import { DataGrid, useGridApiRef, GridToolbar, getGridNumericOperators } from '@
 import userManagementApi from '../../../services/adminApi/userManagementApi';
 import { toast } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
+import { styled } from '@mui/material/styles';
+
+const StyledGridOverlayNoRows = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    '& .no-rows-primary': {
+        fill: theme.palette.mode === 'light' ? '#AEB8C2' : '#3D4751',
+    },
+    '& .no-rows-secondary': {
+        fill: theme.palette.mode === 'light' ? '#E8EAED' : '#1D2126',
+    },
+}));
+
+const StyledGridOverlay = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    '& .no-results-primary': {
+        fill: theme.palette.mode === 'light' ? '#AEB8C2' : '#3D4751',
+    },
+    '& .no-results-secondary': {
+        fill: theme.palette.mode === 'light' ? '#E8EAED' : '#1D2126',
+    },
+}));
+
+function CustomNoRowsOverlay() {
+    return (
+      <StyledGridOverlayNoRows>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          width={96}
+          viewBox="0 0 452 257"
+          aria-hidden
+          focusable="false"
+        >
+          <path
+            className="no-rows-primary"
+            d="M348 69c-46.392 0-84 37.608-84 84s37.608 84 84 84 84-37.608 84-84-37.608-84-84-84Zm-104 84c0-57.438 46.562-104 104-104s104 46.562 104 104-46.562 104-104 104-104-46.562-104-104Z"
+          />
+          <path
+            className="no-rows-primary"
+            d="M308.929 113.929c3.905-3.905 10.237-3.905 14.142 0l63.64 63.64c3.905 3.905 3.905 10.236 0 14.142-3.906 3.905-10.237 3.905-14.142 0l-63.64-63.64c-3.905-3.905-3.905-10.237 0-14.142Z"
+          />
+          <path
+            className="no-rows-primary"
+            d="M308.929 191.711c-3.905-3.906-3.905-10.237 0-14.142l63.64-63.64c3.905-3.905 10.236-3.905 14.142 0 3.905 3.905 3.905 10.237 0 14.142l-63.64 63.64c-3.905 3.905-10.237 3.905-14.142 0Z"
+          />
+          <path
+            className="no-rows-secondary"
+            d="M0 10C0 4.477 4.477 0 10 0h380c5.523 0 10 4.477 10 10s-4.477 10-10 10H10C4.477 20 0 15.523 0 10ZM0 59c0-5.523 4.477-10 10-10h231c5.523 0 10 4.477 10 10s-4.477 10-10 10H10C4.477 69 0 64.523 0 59ZM0 106c0-5.523 4.477-10 10-10h203c5.523 0 10 4.477 10 10s-4.477 10-10 10H10c-5.523 0-10-4.477-10-10ZM0 153c0-5.523 4.477-10 10-10h195.5c5.523 0 10 4.477 10 10s-4.477 10-10 10H10c-5.523 0-10-4.477-10-10ZM0 200c0-5.523 4.477-10 10-10h203c5.523 0 10 4.477 10 10s-4.477 10-10 10H10c-5.523 0-10-4.477-10-10ZM0 247c0-5.523 4.477-10 10-10h231c5.523 0 10 4.477 10 10s-4.477 10-10 10H10c-5.523 0-10-4.477-10-10Z"
+          />
+        </svg>
+        <Box sx={{ mt: 2 }}>No users</Box>
+      </StyledGridOverlayNoRows>
+    );
+  }
+
+function CustomNoResultsOverlay() {
+    return (
+        <StyledGridOverlay>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                width={96}
+                viewBox="0 0 523 299"
+                aria-hidden
+                focusable="false"
+            >
+                <path
+                    className="no-results-primary"
+                    d="M262 20c-63.513 0-115 51.487-115 115s51.487 115 115 115 115-51.487 115-115S325.513 20 262 20ZM127 135C127 60.442 187.442 0 262 0c74.558 0 135 60.442 135 135 0 74.558-60.442 135-135 135-74.558 0-135-60.442-135-135Z"
+                />
+                <path
+                    className="no-results-primary"
+                    d="M348.929 224.929c3.905-3.905 10.237-3.905 14.142 0l56.569 56.568c3.905 3.906 3.905 10.237 0 14.143-3.906 3.905-10.237 3.905-14.143 0l-56.568-56.569c-3.905-3.905-3.905-10.237 0-14.142ZM212.929 85.929c3.905-3.905 10.237-3.905 14.142 0l84.853 84.853c3.905 3.905 3.905 10.237 0 14.142-3.905 3.905-10.237 3.905-14.142 0l-84.853-84.853c-3.905-3.905-3.905-10.237 0-14.142Z"
+                />
+                <path
+                    className="no-results-primary"
+                    d="M212.929 185.071c-3.905-3.905-3.905-10.237 0-14.142l84.853-84.853c3.905-3.905 10.237-3.905 14.142 0 3.905 3.905 3.905 10.237 0 14.142l-84.853 84.853c-3.905 3.905-10.237 3.905-14.142 0Z"
+                />
+                <path
+                    className="no-results-secondary"
+                    d="M0 43c0-5.523 4.477-10 10-10h100c5.523 0 10 4.477 10 10s-4.477 10-10 10H10C4.477 53 0 48.523 0 43ZM0 89c0-5.523 4.477-10 10-10h80c5.523 0 10 4.477 10 10s-4.477 10-10 10H10C4.477 99 0 94.523 0 89ZM0 135c0-5.523 4.477-10 10-10h74c5.523 0 10 4.477 10 10s-4.477 10-10 10H10c-5.523 0-10-4.477-10-10ZM0 181c0-5.523 4.477-10 10-10h80c5.523 0 10 4.477 10 10s-4.477 10-10 10H10c-5.523 0-10-4.477-10-10ZM0 227c0-5.523 4.477-10 10-10h100c5.523 0 10 4.477 10 10s-4.477 10-10 10H10c-5.523 0-10-4.477-10-10ZM523 227c0 5.523-4.477 10-10 10H413c-5.523 0-10-4.477-10-10s4.477-10 10-10h100c5.523 0 10 4.477 10 10ZM523 181c0 5.523-4.477 10-10 10h-80c-5.523 0-10-4.477-10-10s4.477-10 10-10h80c5.523 0 10 4.477 10 10ZM523 135c0 5.523-4.477 10-10 10h-74c-5.523 0-10-4.477-10-10s4.477-10 10-10h74c5.523 0 10 4.477 10 10ZM523 89c0 5.523-4.477 10-10 10h-80c-5.523 0-10-4.477-10-10s4.477-10 10-10h80c5.523 0 10 4.477 10 10ZM523 43c0 5.523-4.477 10-10 10H413c-5.523 0-10-4.477-10-10s4.477-10 10-10h100c5.523 0 10 4.477 10 10Z"
+                />
+            </svg>
+            <Box sx={{ mt: 2 }}>No users found.</Box>
+        </StyledGridOverlay>
+    );
+}
 
 function StatusInputValue(props) {
   const { item, applyValue, focusElementRef } = props;
@@ -281,7 +376,7 @@ const UserList = ({ users, onOpenModal, pageSizeChange, pageSize, page, pageChan
   });
 
   return (
-    <Box component="main" className="p-4" sx={{ width: '100%', overflow: 'auto' }}>
+    <Box className="p-4" sx={{ width: '100%', overflow: 'auto' }}>
       <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
         <Typography sx={{ fontSize: "1.5rem", fontWeight: "600" }}>{t("userList")}</Typography>
         {rowSelectionModel.length ? <Button onClick={handleClickOpen} variant="contained" startIcon={<LockIcon />}>
@@ -368,6 +463,8 @@ const UserList = ({ users, onOpenModal, pageSizeChange, pageSize, page, pageChan
           apiRef={apiRef}
           slots={{
             toolbar: GridToolbar,
+            noResultsOverlay: CustomNoResultsOverlay,
+            noRowsOverlay: CustomNoRowsOverlay,
           }}
           slotProps={{
             pagination: {
@@ -386,6 +483,7 @@ const UserList = ({ users, onOpenModal, pageSizeChange, pageSize, page, pageChan
           onColumnVisibilityModelChange={(newModel) =>
             setColumnVisibilityModel(newModel)
           }
+          disableColumnResize={true}
           localeText={{
             footerRowSelected: (count) =>
               count !== 1
