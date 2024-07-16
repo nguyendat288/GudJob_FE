0cc04123273ccd71490d56d991e8da34fec6a7e1
khanhndhe153638 <khanhndhe153638@fpt.edu.vn>
2024-07-16 11:19:02 +0700

    update category list

--- src/pages/Freelancer/Home/Home.jsx
+++ src/pages/Freelancer/Home/Home.jsx
@@ -1,139 +1,206 @@
-import { Box, Card, CardContent, CardMedia, Divider, Grid, Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import categoryApi from '../../../services/categoryApi'
-import { useNavigate } from 'react-router-dom'
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import categoryApi from '../../../services/categoryApi';
+import { useNavigate } from 'react-router-dom';
 import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
 import HeroSection from '../../../components/HeroSection';
 import TypographyTitle from '../../../components/Typography/TypographyTitle';
 import TypographyHeader from '../../../components/Typography/TypographyHeader';
 import { useTranslation } from 'react-i18next';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 const Home = () => {
-  const [listCategory, setListCategory] = useState([])
-  const navigate = useNavigate()
-  const {t} = useTranslation('home');
+  const [listCategory, setListCategory] = useState([]);
+  const navigate = useNavigate();
+  const { t } = useTranslation('home');
 
   useEffect(() => {
     const getCategory = async () => {
       const res = await categoryApi.GetAllCategory();
-      setListCategory(res)
-    }
-    getCategory()
-  }, [])
-  
+      setListCategory(res);
+    };
+    getCategory();
+  }, [listCategory]);
+
   const handleClick = (id) => {
-    navigate(`/category/${id}`)
-  }
+    navigate(`/category/${id}`);
+  };
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 4,
+    },
+  };
 
   return (
     <>
       <HeroSection />
       <Box m={3}>
         <Box mb={3}>
-          <TypographyHeader title= {t('popular_category' )} /> 
-          
+          <TypographyHeader title={t('popular_category')} />
           <Grid container spacing={3} mt={2}>
-            {listCategory.length !== 0 && listCategory.map((item, index) => (
-              <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-                <Card
-                onClick={(e) => handleClick(item?.id)}
-                  sx={{   
-                    maxWidth: 345,
-                    textDecoration: 'none',
-                    cursor: 'pointer',
-                    borderRadius: 2,
-                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                    transition: 'transform 0.3s, box-shadow 0.3s',
-                    '&:hover': {
-                      transform: 'scale(1.05)',
-                      boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)',
-                    },
-                    '&:focus': {
-                      outline: 'none',
-                      boxShadow: '0 0 0 4px rgba(0, 123, 255, 0.5)',
-                    },
-                    '&:active': {
-                      transform: 'scale(0.95)',
-                      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                    }
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={item?.image}
-                  />
-                  <CardContent>
-                    <TypographyTitle title={item?.categoryName} />
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
+            {listCategory.length !== 0 &&
+              listCategory.map((item, index) => {
+                return (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                    <Card
+                      onClick={(e) => handleClick(item?.id)}
+                      sx={{
+                        maxWidth: 345,
+                        textDecoration: 'none',
+                        cursor: 'pointer',
+                        borderRadius: 2,
+                        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                        transition: 'transform 0.3s, box-shadow 0.3s',
+                        '&:hover': {
+                          transform: 'scale(1.05)',
+                          boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)',
+                        },
+                        '&:focus': {
+                          outline: 'none',
+                          boxShadow: '0 0 0 4px rgba(0, 123, 255, 0.5)',
+                        },
+                        '&:active': {
+                          transform: 'scale(0.95)',
+                          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                        },
+                      }}
+                    >
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={item?.image}
+                      />
+                      <CardContent>
+                        <TypographyTitle title={item?.categoryName} />
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                );
+              })}
           </Grid>
         </Box>
         <Divider />
         <Box mt={3}>
           <TypographyHeader title={t('website_can_help_you_about_?')} />
           <Box display="flex">
-            <Box flex='1' p={3}>
+            <Box flex="1" p={3}>
               <CardMedia
                 component="img"
                 height="350"
-                image='https://cdn.tgdd.vn/Files/2021/07/09/1366892/top-8-loai-tranh-phong-thuy-cho-nguoi-menh-kim-hut-tai-loc-may-man-202107091426133314.jpg'
+                image="https://cdn.tgdd.vn/Files/2021/07/09/1366892/top-8-loai-tranh-phong-thuy-cho-nguoi-menh-kim-hut-tai-loc-may-man-202107091426133314.jpg"
               />
             </Box>
-
-            <Box flex='1' p={3}>
-              <Typography variant="h6" display="flex" alignItems="center" mb={2}>
-                <CheckBoxOutlinedIcon sx={{ color: 'green', mr: 1 }} /> Example context
+            <Box flex="1" p={3}>
+              <Typography
+                variant="h6"
+                display="flex"
+                alignItems="center"
+                mb={2}
+              >
+                <CheckBoxOutlinedIcon sx={{ color: 'green', mr: 1 }} /> Example
+                context
               </Typography>
-              <Typography variant="h6" display="flex" alignItems="center" mb={2}>
-                <CheckBoxOutlinedIcon sx={{ color: 'green', mr: 1 }} /> Example context
+              <Typography
+                variant="h6"
+                display="flex"
+                alignItems="center"
+                mb={2}
+              >
+                <CheckBoxOutlinedIcon sx={{ color: 'green', mr: 1 }} /> Example
+                context
               </Typography>
-              <Typography variant="h6" display="flex" alignItems="center" mb={2}>
-                <CheckBoxOutlinedIcon sx={{ color: 'green', mr: 1 }} /> Example context
+              <Typography
+                variant="h6"
+                display="flex"
+                alignItems="center"
+                mb={2}
+              >
+                <CheckBoxOutlinedIcon sx={{ color: 'green', mr: 1 }} /> Example
+                context
               </Typography>
-              <Typography variant="h6" display="flex" alignItems="center" mb={2}>
-                <CheckBoxOutlinedIcon sx={{ color: 'green', mr: 1 }} /> Example context
+              <Typography
+                variant="h6"
+                display="flex"
+                alignItems="center"
+                mb={2}
+              >
+                <CheckBoxOutlinedIcon sx={{ color: 'green', mr: 1 }} /> Example
+                context
               </Typography>
             </Box>
           </Box>
         </Box>
         <Divider />
         <Box mt={3}>
-          <TypographyHeader title={t('explore_more_features_with_membership')} />
+          <TypographyHeader
+            title={t('explore_more_features_with_membership')}
+          />
           <Box display="flex">
-            <Box flex='1' p={3}>
-              <Typography variant="h6" display="flex" alignItems="center" mb={2}>
+            <Box flex="1" p={3}>
+              <Typography
+                variant="h6"
+                display="flex"
+                alignItems="center"
+                mb={2}
+              >
                 New feature with membership
               </Typography>
-              <Typography variant="h6" display="flex" alignItems="center" mb={2}>
+              <Typography
+                variant="h6"
+                display="flex"
+                alignItems="center"
+                mb={2}
+              >
                 New feature with membership
               </Typography>
-              <Typography variant="h6" display="flex" alignItems="center" mb={2}>
+              <Typography
+                variant="h6"
+                display="flex"
+                alignItems="center"
+                mb={2}
+              >
                 New feature with membership
               </Typography>
-              <Typography variant="h6" display="flex" alignItems="center" mb={2}>
+              <Typography
+                variant="h6"
+                display="flex"
+                alignItems="center"
+                mb={2}
+              >
                 New feature with membership
               </Typography>
             </Box>
-            <Box flex='1' p={3}>
+            <Box flex="1" p={3}>
               <CardMedia
                 component="img"
                 height="350"
-                image='https://cdn.tgdd.vn/Files/2021/07/09/1366892/top-8-loai-tranh-phong-thuy-cho-nguoi-menh-kim-hut-tai-loc-may-man-202107091426133314.jpg'
+                image="https://cdn.tgdd.vn/Files/2021/07/09/1366892/top-8-loai-tranh-phong-thuy-cho-nguoi-menh-kim-hut-tai-loc-may-man-202107091426133314.jpg"
               />
             </Box>
           </Box>
         </Box>
-        {/* <Divider />
-        <Box mt={3}>
-          <Typography fontSize='25px' fontWeight='bold'> What you need we've got it  </Typography>
-          
-        </Box>
-        <Divider /> */}
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://webapp-doan-2.azurewebsites.net'
+export const BASE_URL = 'https://webapp-doan-2.azurewebsites.net';
